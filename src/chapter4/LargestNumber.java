5975174e4bce1654b8fff598dbb8f5e21caf30b2
Frank-dev20 <61961584+Frank-dev20@users.noreply.github.com>
2021-08-02 11:39:10 +0100

    Getting the largest from from user input using sentinel loop

--- src/chapter4/LargestNumber.java
+++ src/chapter4/LargestNumber.java
@@ -1,2 +1,23 @@
-package chapter4;public class LargestNumber {
+package chapter4;
+
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        int count =10;
+        int number;
+        int largest;
+        System.out.println("Enter a number ");
+        number = input.nextInt();
+        largest=number;
+        while(number!=0){
+            System.out.println("enter a number ");
+            number = input.nextInt();
+            if(number>largest) {
+                largest = number;
+            }
+        }
+        System.out.println(largest);
+    }
 }
