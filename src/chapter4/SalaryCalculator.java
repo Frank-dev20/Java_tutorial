87e9b993d70240507167d75d24cab1f5fb6d4ad7
Frank-dev20 <61961584+Frank-dev20@users.noreply.github.com>
2021-08-02 11:40:39 +0100

    calculating employee salary

--- src/chapter4/SalaryCalculator.java
+++ src/chapter4/SalaryCalculator.java
@@ -1,2 +1,24 @@
-package chapter4;public class SalaryCalculator {
+package chapter4;
+
+import java.util.Scanner;
+
+public class SalaryCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int workedHours_1;
+        int hourlyRate;
+        int grossPay;
+        System.out.println("Enter employee name ");
+        String employee1_Name = input.next();
+
+        System.out.println("Enter employee hours worked ");
+        workedHours_1 = input.nextInt();
+
+        System.out.println("Enter employee hourly rate ");
+        hourlyRate = input.nextInt();
+
+        grossPay = workedHours_1 * hourlyRate;
+
+        System.out.println("Employee's name is " + employee1_Name + " " + "And his gross pay is " + grossPay);
+    }
 }
