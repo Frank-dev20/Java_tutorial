eb7b4c25c0b9ea885237605197e2b3f3dd4bbeec
Frank-dev20 <61961584+Frank-dev20@users.noreply.github.com>
2021-11-01 04:59:49 +0100

    Main class added

--- src/article/oop/Main.java
+++ src/article/oop/Main.java
@@ -1,2 +1,14 @@
-package article.oop;public class Main {
+package article.oop;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Toyota toyota = new Toyota();
+        Car car = new Toyota();
+        toyota.accelerate();
+    }
+
+
 }
